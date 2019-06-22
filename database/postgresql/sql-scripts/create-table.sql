d0dae2c2c2f2263cc263b5247215b8d7252d1cb0
Nan Qin <qinnan0416@gmail.com>
2019-06-22 18:11:55 -0400

    add pl/pgsql

--- database/postgresql/sql-scripts/create-table.sql
+++ database/postgresql/sql-scripts/create-table.sql
@@ -1,1 +1,9 @@
-CREATE SCHEMA schema_name;
+create or replace function query_actor(skip_ integer, limit_ integer)
+    returns table(firstname character varying(45),
+                 lastname character varying(45))
+as $$
+begin
+    return query select first_name, last_name from public.actor limit limit_ offset skip_;
+end; 
+$$
+LANGUAGE 'plpgsql';
